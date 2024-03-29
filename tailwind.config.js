0e912e6bf614f0655a4615cc21d58185fdd1a849
Matthew Fenn <matt@mooloop.com>
2024-03-29 11:13:05 +0000

    switched from cdn to cli

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandcolour: '#4CA39B',
+        bgblue: '#130B35',
+        bgltblue: '#F4C8F0',
+        bgyellow: '#F7C433',
+        bgbluetint: '#E7E6EA'
+      }
+    },
+    fontFamily: {
+      'mono': ['"American Typewriter", "Courier New"', 'monospace'],
+      'custom': ['YourCustomFont', 'sans-serif'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
